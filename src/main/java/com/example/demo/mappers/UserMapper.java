4aed6f169311090acfc5e46f1d591580667b766a
Milan <pronicmilan@gmail.com>
2024-05-17 02:13:02 +0200

    insert in db problem

--- src/main/java/com/example/demo/mappers/UserMapper.java
+++ src/main/java/com/example/demo/mappers/UserMapper.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserMapper {
-
+    //ovde je problem
     public static User toEntity(UserModel model) {
         User user = new User();
         user.setId(model.getId());
